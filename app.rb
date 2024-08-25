380536c8aaca5f7659515c3e02af296da45090ea
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2024-08-25 15:11:26 -0500

    Add indentation in Ruby code

--- app.rb
+++ app.rb
@@ -6,21 +6,21 @@ task_list = []
 id_counter = 1
 
 def overdue?(due_date)
-Date.parse(due_date) < Date.today
+  Date.parse(due_date) < Date.today
 end
 
 get '/' do
-@tasks = task_list
-erb :index
+  @tasks = task_list
+  erb :index
 end
 
 post '/add' do
-task_list << {id: id_counter, name: params[:task], due_date: params[:due_date]}
-id_counter += 1
-redirect '/'
+  task_list << {id: id_counter, name: params[:task], due_date: params[:due_date]}
+  id_counter += 1
+  redirect '/'
 end
 
 post '/delete/:id' do
-task_list.reject! { |task| task[:id] == params[:id].to_i }
-redirect '/'
+  task_list.reject! { |task| task[:id] == params[:id].to_i }
+  redirect '/'
 end
