8767f6ed665f8dd62fe6a2e4f16bea4df4e60070
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2024-08-25 16:06:21 -0500

    Add class to fix warnings

--- app.rb
+++ app.rb
@@ -2,25 +2,29 @@ require 'sinatra'
 require 'erb'
 require 'date'
 
-task_list = []
-id_counter = 1
 
-def overdue?(due_date)
-  Date.parse(due_date) < Date.today
-end
 
-get '/' do
-  @tasks = task_list
-  erb :index
-end
+class TaskList < Sinatra::Base
+  task_list = []
+  id_counter = 1
+  def overdue?(due_date)
+    Date.parse(due_date) < Date.today
+  end
 
-post '/add' do
-  task_list << {id: id_counter, name: params[:task], due_date: params[:due_date]}
-  id_counter += 1
-  redirect '/'
-end
+  get '/' do
+    @tasks = task_list
+    erb :index
+  end
 
-post '/delete/:id' do
-  task_list.reject! { |task| task[:id] == params[:id].to_i }
-  redirect '/'
+  post '/add' do
+    task_list << {id: id_counter, name: params[:task], due_date: params[:due_date]}
+    id_counter += 1
+    redirect '/'
+  end
+
+  post '/delete/:id' do
+    task_list.reject! { |task| task[:id] == params[:id].to_i }
+    redirect '/'
+  end
 end
+TaskList.run!
