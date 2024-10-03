d70d1c5511eda048f68eeea3311b738664030678
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2024-10-03 16:00:40 -0500

    Add comments and format code

--- app.rb
+++ app.rb
@@ -1,30 +1,30 @@
-require 'sinatra' # Sinatra web server
-require 'erb' # Ruby templates
-require 'date' # date parser
+require "sinatra"
+require "erb"
+require "date"
 
-class TaskList < Sinatra::Base # task list class
-  task_list = [] # set initial task list to empty array
-  id_counter = 1 # set initial id counter to 1
+class TaskList < Sinatra::Base
+  task_list = []
+  id_counter = 1
 
-  def overdue?(due_date) # check if task is overdue
-    Date.parse(due_date) < Date.today # parse the date string
+  def overdue?(due_date) #function to check if task is overdue
+    Date.parse(due_date) < Date.today
   end
 
-  get '/' do # get task list
-    @tasks = task_list # set tasks to task list array
-    erb :index # redirect to index page
+  get "/" do #get request from index page (home page)
+    @tasks = task_list
+    erb :index
   end
 
-  post '/add' do # add task to task list
-    task_list << { id: id_counter, name: params[:task], due_date: params[:due_date] } # add task to task list
-    id_counter += 1 # increase id counter by 1 when task is added
-    redirect '/' # redirect to index page
+  post "/add" do #post request to add task to task list
+    task_list << { id: id_counter, name: params[:task], due_date: params[:due_date] } #add task based on entered form inputs
+    id_counter += 1 #increase id counter by 1
+    redirect "/" #redirect to index page (home page)
   end
 
-  post '/delete/:id' do
-    task_list.reject! { |task| task[:id] == params[:id].to_i } # find task to delete by task id
-    redirect '/' # redirect to index page
+  post "/delete/:id" do #post request to delete task from task id
+    task_list.reject! { |task| task[:id] == params[:id].to_i }
+    redirect "/" #redirect to index page (home page)
   end
 end
 
-TaskList.run! # run the task list application
+TaskList.run! #run the task list app
